b4198c4df7dcf8943a026372aab8fbdbda190dbe
Luka <167115889+Lule1998@users.noreply.github.com>
2024-11-12 14:27:14 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-// src/app/app.component.ts
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './components/users/users.component';
