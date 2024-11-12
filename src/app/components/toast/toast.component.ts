0ab077543afdaaf3d14255b0a6d792bc525e599a
Luka <167115889+Lule1998@users.noreply.github.com>
2024-11-12 14:26:35 +0100

    Update toast.component.ts

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,4 +1,3 @@
-// src/app/components/toast/toast.component.ts
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastService } from '../../services/toast.service';
